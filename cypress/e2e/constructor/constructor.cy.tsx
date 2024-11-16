3fd5dffe5fc0b87b486f6863b4bc484357f95138
shiyuu <zaline1337@yandex.by>
2024-11-16 16:42:31 +0300

    исправил замечания ревьюера

--- cypress/e2e/constructor/constructor.cy.tsx
+++ cypress/e2e/constructor/constructor.cy.tsx
@@ -1,7 +1,12 @@
 /// <reference types="cypress" />
 
+const testUrl = 'http://localhost:8080/';
+const testIngredientBun = '643d69a5c3f7b9001cfa093c';
+const testIngredientMain = '643d69a5c3f7b9001cfa0941';
+const testIngredientSauce = '643d69a5c3f7b9001cfa0942';
+
 beforeEach(() => {
-  cy.visit('http://localhost:8080/');
+  cy.visit(testUrl);
   cy.intercept('GET', 'api/ingredients', {
     fixture: 'ingredients.json'
   });
@@ -9,29 +14,29 @@ beforeEach(() => {
 
 describe('Добавление ингредиента из списка ингредиентов в конструктор', () => {
   it('Добавление булки в конструктор', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa093c]').contains('Добавить').click();
+    cy.get(`[data-cy=${testIngredientBun}]`).contains('Добавить').click();
     cy.get('[data-cy=burger-bun]').should('exist');
   });
 
   it('Добавление ингредиента в конструктор', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa0941]').contains('Добавить').click();
+    cy.get(`[data-cy=${testIngredientMain}]`).contains('Добавить').click();
     cy.get('[data-cy=burger-item]').should('exist');
   });
 
   it('Добавление соуса в конструктор', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa0942]').contains('Добавить').click();
+    cy.get(`[data-cy=${testIngredientSauce}]`).contains('Добавить').click();
     cy.get('[data-cy=burger-item]').should('exist');
   });
 });
 
 describe('Открытие и закрытие модального окна с описанием ингредиента', () => {
   it('Открыть модальное окно', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa093c]').click();
+    cy.get(`[data-cy=${testIngredientBun}]`).click();
     cy.get('[data-cy=modal]').should('exist');
   });
 
   it('Закрыть модальное окно', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa093c]').click();
+    cy.get(`[data-cy=${testIngredientBun}]`).click();
     cy.get('[data-cy=modal]').should('exist');
     cy.get('[data-cy=modal_close-btn]').click();
     cy.get('[data-cy=modal]').should('not.exist');
@@ -60,11 +65,16 @@ describe('Процесс создания заказа', () => {
   });
 
   it('Создание заказа', () => {
-    cy.get('[data-cy=643d69a5c3f7b9001cfa093c]').contains('Добавить').click();
-    cy.get('[data-cy=643d69a5c3f7b9001cfa0941]').contains('Добавить').click();
-    cy.get('[data-cy=643d69a5c3f7b9001cfa0942]').contains('Добавить').click();
+    cy.get(`[data-cy=${testIngredientBun}]`).contains('Добавить').click();
+    cy.get(`[data-cy=${testIngredientMain}]`).contains('Добавить').click();
+    cy.get(`[data-cy=${testIngredientSauce}]`).contains('Добавить').click();
 
-    cy.get('[data-cy=order-summ]').contains('Оформить заказ').click();
+    cy.get('[data-cy=order-summ]')
+      .contains('Оформить заказ')
+      .click()
+      .intercept('POST', 'api/orders', {
+        fixture: 'order.json'
+      });
 
     cy.get('[data-cy=modal]').contains('57156').should('exist');
 

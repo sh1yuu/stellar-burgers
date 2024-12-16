fd74fcba7cf8f2d01c48dc1d34563d13861906b2
Shukanov Mikhail <zaline1337@yandex.by>
2024-12-16 21:40:00 +0300

    fix: фиксы к деплою

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
